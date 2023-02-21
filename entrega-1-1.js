37c3886f1f54e6196dd25dac4f96c5e61c99b758
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-21 09:57:40 +0100

    add nivell 2 - ex 1

--- entrega-1-1.js
+++ entrega-1-1.js
@@ -4,3 +4,12 @@ Exercici 1: Crea una funció que mostri per consola el nom d'usuari/ària en inv
 const showName = (name) => console.log(name);
 
 showName("Berenguer");
+
+/* Nivell 2
+Exercici 1: Mostra per consola el nom i cognoms de l'usuari/ària mitjançant template literals, guardant-los en variables i referenciant-les en la impressió del missatge. */
+
+const personName = "Berenguer";
+const personSurname = "Pou Llavina";
+
+console.log(`Name: ${personName}
+Surname: ${personSurname}`);
