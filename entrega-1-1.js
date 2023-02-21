4d7ddbfd0f9b6ca986239cb79f3f7a07004f1255
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-21 09:43:41 +0100

    add nivell 1 - exercici 1 showName

---
+++ entrega-1-1.js
@@ -0,0 +1,6 @@
+/* Nivell 1
+Exercici 1: Crea una funció que mostri per consola el nom d'usuari/ària en invocar-la passant-li el nom com a paràmetre. */
+
+const showName = (name) => console.log(name);
+
+showName("Berenguer");
