04eda5e014345784fcd0322852313ca6695f86e7
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-22 09:53:23 +0100

    add 1.2 nivell 1 ex 1

---
+++ entrega-1-2.js
@@ -0,0 +1,4 @@
+/* Nivell 1
+- Exercici 1: Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres. */
+
+console.log(((firstNumber, secondNumber) => firstNumber + secondNumber)(3, 7));
