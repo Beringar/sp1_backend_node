9ec36f93e72f997366a840f253ef7edc2e72af87
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-22 10:16:17 +0100

    add 1.2 nivell 2 ex 1

--- entrega-1-2.js
+++ entrega-1-2.js
@@ -2,3 +2,12 @@
 - Exercici 1: Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres. */
 
 console.log(((firstNumber, secondNumber) => firstNumber + secondNumber)(3, 7));
+
+/* Nivell 2
+- Exercici 1: Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut. */
+
+const createObject = (value) => ({
+  key: value,
+});
+
+console.log(createObject("Backend Node.js"));
