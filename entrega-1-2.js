6dd91b147f02a9b6a59ecfee22741afe64db2182
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-22 11:04:29 +0100

    add 1.2 nivell 2 ex 2

--- entrega-1-2.js
+++ entrega-1-2.js
@@ -7,7 +7,24 @@ console.log(((firstNumber, secondNumber) => firstNumber + secondNumber)(3, 7));
 - Exercici 1: Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut. */
 
 const createObject = (value) => ({
-  key: value,
+  nom: value,
 });
 
 console.log(createObject("Backend Node.js"));
+
+/* Nivell 2
+- Exercici 2: Crea una classe "Persona" que rebi un paràmetre 'nom' en ser instanciada. La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. Invoca el mètode dirNom des de fora de la classe. */
+
+class Persona {
+  constructor(nom) {
+    this.nom = nom;
+  }
+
+  dirNom() {
+    console.log(this.nom);
+  }
+}
+
+const persona = new Persona("Berenguer");
+
+persona.dirNom();
