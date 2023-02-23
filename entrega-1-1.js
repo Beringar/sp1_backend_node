2ac947b138b7c252f8a0fcde4e8dce7027c71bc2
Berenguer Pou <berenguer.pou@gmail.com>
2023-02-23 10:59:09 +0100

    add 1.2 nivell 3 ex1 ex2 / log descriptions

--- entrega-1-1.js
+++ entrega-1-1.js
@@ -1,12 +1,16 @@
 /* Nivell 1
 Exercici 1: Crea una funció que mostri per consola el nom d'usuari/ària en invocar-la passant-li el nom com a paràmetre. */
 
+console.log("---- nivell 1 - ex 1 ----");
+
 const showName = (name) => console.log(name);
 
 showName("Berenguer");
 
 /* Nivell 2
 Exercici 1: Mostra per consola el nom i cognoms de l'usuari/ària mitjançant template literals, guardant-los en variables i referenciant-les en la impressió del missatge. */
+
+console.log("---- nivell 2 - ex 1 ----");
 
 const personName = "Berenguer";
 const personSurname = "Pou Llavina";
@@ -16,6 +20,8 @@ Surname: ${personSurname}`);
 
 /* Nivell 2
 Exercici 2: Invoca una funció que retorni un valor des de dins d'una template literal. */
+
+console.log("---- nivell 2 - ex 2 ----");
 
 const getPhraseLength = (phrase) => {
   return phrase.length;
@@ -32,6 +38,8 @@ console.log(
 /* Nivell 3
 Exercici 1: Crea una matriu de deu funcions i emplena-la mitjançant un bucle de manera que cada funció compti del 0 al 9 per la consola. Invoca cada funció de l'array iterativament. Haurà de mostrar-se per consola el compte del 0 al 9 deu vegades. */
 
+console.log("---- nivell 3 - ex 1 ----");
+
 const countersArray = new Array(10).fill(() => {
   for (let i = 0; i < 10; i++) {
     console.log(i);
@@ -44,5 +52,7 @@ countersArray.forEach((countFunction) => {
 
 /* Nivell 3
 Exercici 2: Crea una funció anònima autoinvocable igualada a una variable que mostri per consola el nom de l'usuari/ària a rebut com a paràmetre. */
+
+console.log("---- nivell 3 - ex 2 ----");
 
 const userNameResolved = ((userName) => console.log(userName))("Berenguer");
--- entrega-1-2.js
+++ entrega-1-2.js
@@ -1,10 +1,14 @@
 /* Nivell 1
 - Exercici 1: Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres. */
 
+console.log("---- nivell 1 - ex 1 ----");
+
 console.log(((firstNumber, secondNumber) => firstNumber + secondNumber)(3, 7));
 
 /* Nivell 2
 - Exercici 1: Crea una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut. */
+
+console.log("---- nivell 2 - ex 1 ----");
 
 const createObject = (value) => ({
   nom: value,
@@ -14,6 +18,8 @@ console.log(createObject("Backend Node.js"));
 
 /* Nivell 2
 - Exercici 2: Crea una classe "Persona" que rebi un paràmetre 'nom' en ser instanciada. La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. Invoca el mètode dirNom des de fora de la classe. */
+
+console.log("---- nivell 2 - ex 2 ----");
 
 class Persona {
   constructor(nom) {
@@ -28,3 +34,44 @@ class Persona {
 const persona = new Persona("Berenguer");
 
 persona.dirNom();
+
+/* Nivell 3
+- Exercici 1
+Escriu una function creadora d'objectes que faci instàncies d'una classe abstracta. Invoca-la amb diferents definicions. */
+
+console.log("---- nivell 3 - ex 1 ----");
+
+class AbstractClass {
+  constructor(nom) {
+    if (new.target === AbstractClass) {
+      throw new TypeError("Cannot construct Abstract instances directly");
+    }
+    this.nom = nom;
+  }
+
+  dirNom() {
+    console.log(`El meu nom és: ${this.nom}`);
+  }
+}
+// funció generadora objectes de la classe abstracta
+const objectAbstractFactory = (nom) =>
+  new (class extends AbstractClass {})(nom);
+
+const objectA = objectAbstractFactory("Berenguer");
+const objectB = objectAbstractFactory("Maria");
+console.log(objectA);
+console.log(objectB);
+
+// proposta Omar, fent servir prototype
+const objectAbstractPrototype = (nom) => {
+  const obj = Object.create(AbstractClass.prototype);
+  obj.nom = nom;
+  return obj;
+};
+
+const objectC = objectAbstractPrototype("Omar");
+console.log(objectC);
+
+objectA.dirNom();
+objectB.dirNom();
+objectC.dirNom();
