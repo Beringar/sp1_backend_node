517a00ac6a85ad6ecc9839f5fe3d750cf5909de1
Berenguer Pou <berenguer.pou@gmail.com>
2023-03-05 18:11:25 +0100

    add 1.5 n2e1

---
+++ 1_5_node_utils/n2e1.js
@@ -0,0 +1,14 @@
+/* Nivell 2
+Exercici 1: Crea una funció que imprimeixi recursivament un missatge per la consola amb demores d'un segon.
+*/
+
+const printMessageEverySec = (message, times) => {
+  let count = 0;
+  const printerInterval = setInterval(() => {
+    console.log(message);
+    count++;
+    if (count === times) clearInterval(printerInterval);
+  }, 1000);
+};
+
+printMessageEverySec("This is from N2E1", 5);
