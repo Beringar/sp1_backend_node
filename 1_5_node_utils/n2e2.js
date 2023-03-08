0d52c5641867b221cc285b211ea95b46ebd9b19a
Berenguer Pou <berenguer.pou@gmail.com>
2023-03-08 12:19:09 +0100

    refactor n2e2 to handle win32 or mac/linux platforms

--- 1_5_node_utils/n2e2.js
+++ 1_5_node_utils/n2e2.js
@@ -1,15 +1,14 @@
 const { exec } = require("child_process");
+const os = require("os");
 
 /*
 Nivell 2
 Exercici 2: Crea una funció que llisti per la consola el contingut del directori d'usuari/ària de l'ordinador utilizant Node Child Processes.
-
-ATENCIÓ!!!: el command és per a linux/macos.
-Si executeu aquest arxiu a windows heu de posar com a primer paràmetre de exec --> "cd ~ && dir"
  */
 
 const showUserFiles = () => {
-  exec("cd ~ && ls -la", (err, stdout, stderr) => {
+  const command = os.platform() === "win32" ? "cd ~ && dir" : "cd ~ && ls -la"; //defineixo el command en funció de si és shell de windows o bé de mac/linux
+  exec(command, (err, stdout, stderr) => {
     if (err) {
       console.error(`exec error: ${err}`);
       return;
