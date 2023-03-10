7addeeac31e25c01c328de817d4efcab9bb6ad8f
Berenguer Pou <berenguer.pou@gmail.com>
2023-03-10 14:30:13 +0100

    add tests 1.6 nivell 2 class Persona method dirNom

---
+++ 1_6_testing/app/Persona.js
@@ -0,0 +1,16 @@
+/*
+Nivell 2
+- Exercici 2: Crea una classe "Persona" que rebi un paràmetre 'nom' en ser instanciada. La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. Invoca el mètode dirNom des de fora de la classe.
+*/
+
+class Persona {
+  constructor(nom) {
+    this.nom = nom;
+  }
+
+  dirNom() {
+    return this.nom;
+  }
+}
+
+module.exports = Persona;
